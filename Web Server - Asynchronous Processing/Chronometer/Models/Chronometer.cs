e1e89009a91ab38c5e6dad0b2e43cde0a94363d0
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-05-26 17:01:38 +0300

    add custom chronometer

---
+++ Web Server - Asynchronous Processing/Chronometer/Models/Chronometer.cs
@@ -0,0 +1,86 @@
+﻿using Chronometer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Chronometer.Models
+{
+    public class Chronometer : IChronometer
+    {
+        private long startTime;
+        private long elapsedTime;
+        private bool isStopped;
+
+        public Chronometer()
+        {
+            this.Laps = new List<string>();
+        }
+
+        public string GetTime => GetConvertedTime();
+
+        public List<string> Laps { get; set; }
+
+        public string Lap()
+        {
+            var lap = this.GetTime;
+            this.Laps.Add(lap);
+
+            if (!isStopped)
+            {
+                this.Start();
+            }
+
+            return lap;
+        }
+
+        public void Reset()
+        {
+            this.Stop();
+            this.elapsedTime = 0;
+            this.Laps.Clear();
+        }
+
+        public void Start()
+        {
+            this.startTime = GetCurrentTime();
+            isStopped = false;
+        }
+
+        public void Stop()
+        {
+            if (!isStopped)
+            {
+                UpdateElapsedTime();
+            }
+
+            isStopped = true;
+        }
+
+        private long GetCurrentTime() 
+            => DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+        private string GetConvertedTime()
+        {
+            if (!isStopped)
+            {
+                UpdateElapsedTime();
+                this.Start();
+            }
+
+            TimeSpan t = TimeSpan.FromMilliseconds(this.elapsedTime);
+            string result = string.Format("{0:D2}:{1:D2}:{2:D4}",
+                                    t.Minutes,
+                                    t.Seconds,
+                                    t.Milliseconds);
+
+            return result;
+        }
+
+        private void UpdateElapsedTime()
+        {
+            var currentTime = GetCurrentTime();
+            this.elapsedTime += (currentTime - this.startTime);
+        }
+    }
+}
---
+++ Web Server - Asynchronous Processing/Chronometer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+using Chronometer.Models;
+
+namespace Chronometer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chronometer = new Chronometer.Models.Chronometer();
+
+            while (true)
+            {
+                var line = Console.ReadLine().ToLower();
+
+                switch (line)
+                {
+                    case "start": chronometer.Start(); break;
+                    case "stop": chronometer.Stop(); break;
+                    case "time": Console.WriteLine(chronometer.GetTime); break;
+                    case "reset": chronometer.Reset(); break;
+                    case "exit": return;
+
+                    case "lap":
+                        {
+                            chronometer.Lap();
+                            Console.WriteLine(chronometer.Laps.LastOrDefault());
+                        }
+                        break;
+
+                    case "laps":
+                        {
+                            for (int i = 0; i < chronometer.Laps.Count; i++)
+                            {
+                                Console.WriteLine($"{i + 1}. {chronometer.Laps[i]}");
+                            }
+                        }
+                        break;                  
+                }
+            }
+        }
+    }
+}
